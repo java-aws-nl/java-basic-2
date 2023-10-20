f62e10a669c53ebd76d66515384a769b329a4640
java-aws-nl <rajithabizzd@gmail.com>
2023-10-20 12:55:56 +0200

    new change2

--- src/main/java/nl/pr/demo/Hello.java
+++ src/main/java/nl/pr/demo/Hello.java
@@ -2,6 +2,6 @@ package nl.pr.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Change the code");
+        System.out.println("Change the code2");
     }
 }
